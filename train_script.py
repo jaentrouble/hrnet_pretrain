18ada6b1739dc0abbd6a7b90dd73dd099c541646
jaentrouble <45917844+jaentrouble@users.noreply.github.com>
2020-09-17 01:46:57 +0900

    changed val_label

--- train_script.py
+++ train_script.py
@@ -30,31 +30,8 @@ if args.mem_growth:
         except RuntimeError as e:
             print(e)
 
-data_dir = Path('data')
-data_groups = next(os.walk(data_dir))[1]
-img = []
-data = []
-img_name_dict = {}
-img_idx = 0
-for dg in data_groups[:]:
-    img_dir = data_dir/dg/'done'
-    img_names = os.listdir(img_dir)
-    for name in img_names:
-        img_path = str(img_dir/name)
-        img.append(io.imread(img_path))
-        img_name_dict[img_path] = img_idx
-        img_idx += 1
+train_dir = '/home/jaentrouble/data/imagenet/train'
 
-    json_dir = data_dir/dg/'save'
-    json_names = os.listdir(json_dir)
-    dg_data = []
-    for name in json_names[:]:
-        with open(str(json_dir/name),'r') as j:
-            dg_data.extend(json.load(j))
-    for dg_datum in dg_data :
-        long_img_name = str(img_dir/dg_datum['image'])
-        dg_datum['image'] = img_name_dict[long_img_name]
-    data.extend(dg_data)
 
 test_num = len(data) // 10
 # To make sure data are chosen randomly between data groups
